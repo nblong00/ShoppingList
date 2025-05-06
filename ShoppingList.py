4fadb5348ce3d9f756c3a276d772073190b6a0a6
Blaise <blazzze0517@gmail.com>
2025-05-06 14:00:00 -0400

    Updated logic and added 'show list' function

--- ShoppingList.py
+++ ShoppingList.py
@@ -1,33 +1,41 @@
-# Create a new empty list named shopping_list
 
 shopping_list = []
 
-# Create function named add_to_list that declares a parameter named item
-    # Add item to list
-def add_to_list(item):
-    shopping_list.append(item) 
-    print("{} has been added to the list".format(item))
-    print("Shopping list currently contains {} item(s).".format(len(shopping_list)))
-    #notify user item was added
-    #state number of items in list
-    
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items. 
 Enter 'HELP' for this help.
+Enter 'LIST' to list current items added.
 """)
     
+    
+def add_to_list(item):
+    shopping_list.append(item) 
+    print("{} has been added to the list".format(item))
+    print("Shopping list currently contains {} item(s).".format(len(shopping_list)))
+    
+    
+def show_list():
+    
+    item_number = 1
+    print("Here is your current list:")
+    for item in shopping_list:
+        print("{}. {}".format(item_number, item))
+        item_number += 1
+    
+    
 show_help()
-
 while True:
     new_item = input("> ")
-    
     if new_item.upper() == "DONE":
         break
     elif new_item.lower() == "help":
         show_help()
         continue
-          
+    elif new_item.lower() == "list":
+        show_list()
+        continue  
     add_to_list(new_item.title())
         
