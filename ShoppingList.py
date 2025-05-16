3ca9ff6cac058e892090de820708b42d2787740f
Blaise <blazzze0517@gmail.com>
2025-05-16 14:00:19 -0400

    Consolidated shopping_list, and prices into one list. Now importing CSV on start

--- ShoppingList.py
+++ ShoppingList.py
@@ -1,10 +1,18 @@
 import datetime
+import csv
 
 shopping_list = []
-item_prices = []
 
 
 def welcome(name):
+
+    with open('data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            shopping_list.append({
+                'item': row['item'],
+                'cost': row['cost'],
+            })
 
     dt = datetime.datetime.now()
 
@@ -13,7 +21,7 @@ def welcome(name):
     print(f"Today's date is: {dt.strftime("%B %d, %Y")}")
     print("Welcome to the shopping list simulator!")
     print()
-
+    print(shopping_list)
 
 def show_help(loop_runs):
     if loop_runs < 1:
@@ -53,6 +61,11 @@ def show_list():
     for index, item in enumerate(shopping_list, 1):
         item_index = shopping_list.index(item)
         print(f"{index}. {item} - ${item_prices[item_index]}")
+
+def save_list():
+    with open('data.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow([shopping_list, item_prices])
     
 def main():
 
@@ -66,6 +79,7 @@ def main():
             loop_passes += 1
         new_item = input("> ")
         if new_item.upper() == "DONE":
+            save_list()
             break
         elif new_item.lower() == "help":
             show_help(loop_passes)
